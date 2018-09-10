6dc4c75a42576cdea49e34afc372b85bfac30984
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-09-10 17:08:17 -0400

    trying to move dart

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let dart = document.getElementsByClassName('dart')[0];
 let body = document.getElementsByTagName('body')[0];
 
-let rotation = 180;
-dart.style.transform = `rotate(${rotation}deg)`;
+dart.style.bottom = '100px';
+dart.style.left = '70px';
+dart.style.transform = `rotate(180deg)`;
 
 let aimInterval = setInterval(() => {
-    console.log('in aimInterval');
     let rotationString = dart.style.transform;
-    console.log('rotationString:', rotationString);
     console.log(rotationString.match(/\d+/)[0]);
     let rotation = Number(rotationString.match(/\d+/)[0]);
-    console.log('rotation:', rotation);
     rotation += 1;
     dart.style.transform = `rotate(${rotation}deg)`;
+    console.log('rotation:', rotation);
 }, 15);
 
 body.addEventListener('click', stopAimingDart);
@@ -27,7 +26,6 @@ let startTime = Date.now();
 
 function windUpDart() {
     let windUpInterval = setInterval(() => {
-        console.log('in windUpInterval');
         let timeElapsed = Date.now() - startTime;
         draw(timeElapsed);
     }, 20);
@@ -37,7 +35,22 @@ function windUpDart() {
 }
 
 function draw(timeElapsed) {
-    console.log(dart.style.left);
-    dart.style.left = timeElapsed / 10 + 'px';
-    dart.style.bottom = timeElapsed / 10 + 'px';
+    let rotationString = dart.style.transform;
+    let rotation = Number(rotationString.match(/\d+/)[0]);
+    let angle = rotation - 180;
+    let ratio = Math.tan(angle);
+    console.log('rotation:', rotation, 'angle:', angle, 'ratio:', ratio);
+    let left = dart.style.left;
+    let bottom = dart.style.bottom;
+    console.log('left:', left, 'bottom:', bottom);
+
+    // let leftNumber = Number(dart.style.left.match(/\d+/)[0]);
+    // console.log('leftNumber:', leftNumber);
+
+    dart.style.left = left * ratio + 'px';
+    dart.style.bottom = bottom * ratio + 'px';
+    console.log('new left:', dart.style.left, 'new bottom:', dart.style.bottom);
+
+    // dart.style.left = timeElapsed / 10 + 'px';
+    // dart.style.bottom = timeElapsed / 10 + 'px';
 }
