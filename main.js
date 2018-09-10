82bde1f2f8c07651fb642b8bef01dc642871736a
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-09-10 19:11:00 -0400

    fixed src

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function displayMessage() {
     messageDiv.id = 'message-div';
     messageDiv.textContent = 'HAPPY BIRTHDAY!';
     let cakeImage = document.createElement('img');
-    cakeImage.src = './birthday-cake-2.jpg';
+    cakeImage.src = './birthday-cake-2.JPG';
     cakeImage.id = 'cake-image';
     messageDiv.appendChild(cakeImage);
     body.appendChild(messageDiv);
